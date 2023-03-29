2a7448d0eb1c98d54977e4c75d0ab4c4cfd22461
Stalin <sta_vla411@hotmail.com>
2023-03-29 18:31:43 -0500

    add components to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { dark, light } from './styles/Theme'
+import GlobalStyles from './styles/GlobalStyles'
+import useThemeMode from './hooks/useThemeMode'
+import Dashboard from './routes/Dashboard'
+import Login from './routes/Login'
 function App() {
-  return <div className='App'>hellow world</div>
+  const [theme, toggleTheme] = useThemeMode()
+  return (
+    <>
+      <GlobalStyles theme={theme === 'dark' ? dark : light} />
+      <ThemeProvider theme={theme === 'dark' ? dark : light}>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default App
