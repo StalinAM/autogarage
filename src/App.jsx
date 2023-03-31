76645a91c30f8dae590a5b1e63d3bfdfe6fa02a8
Stalin <sta_vla411@hotmail.com>
2023-03-31 11:47:34 -0500

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { dark, light } from './styles/Theme'
 import GlobalStyles from './styles/GlobalStyles'
 import useThemeMode from './hooks/useThemeMode'
 import Dashboard from './routes/Dashboard'
-import Login from './routes/Login'
+import SignIn from './routes/SignIn'
+import SignUp from './routes/SignUp'
 function App() {
   const [theme, toggleTheme] = useThemeMode()
   return (
@@ -12,7 +13,8 @@ function App() {
       <GlobalStyles theme={theme === 'dark' ? dark : light} />
       <ThemeProvider theme={theme === 'dark' ? dark : light}>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
           <Route path='/dashboard' element={<Dashboard />} />
         </Routes>
       </ThemeProvider>
