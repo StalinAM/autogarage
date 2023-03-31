a706d92a3340e8a303c151113304b5b6d9ca35a2
Stalin <sta_vla411@hotmail.com>
2023-03-31 11:45:50 -0500

    add bax shadow

--- src/styles/Theme.jsx
+++ src/styles/Theme.jsx
@@ -2,11 +2,13 @@ export const dark = {
   elements: 'hsl(209, 23%, 22%)',
   background: 'hsl(207, 26%, 17%)',
   text: 'hsl(0, 0%, 100%)',
-  input: 'hsl(0, 0%, 98%)'
+  input: 'hsl(0, 0%, 98%)',
+  boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px'
 }
 export const light = {
   elements: 'hsl(0, 0%, 100%)',
   background: 'hsl(0, 0%, 98%)',
   text: 'hsl(0, 0%,0%)',
-  input: 'hsl(0, 0%, 52%)'
+  input: 'hsl(0, 0%, 52%)',
+  boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px'
 }
