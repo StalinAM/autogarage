b76a0fb7190e2c81c7ed2361e00cd255fe787167
Stalin <sta_vla411@hotmail.com>
2023-04-01 15:29:16 -0500

    add component search

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+import Header from '../components/Header'
+import AddCar from '../components/FormCar'
+import Search from '../components/Search'
+import Card from '../components/Card'
 
-function Dashboard() {
-  return <div>Dashboard</div>
+function Dashboard({ theme, toggleTheme }) {
+  return (
+    <>
+      <Header theme={theme} toggleTheme={toggleTheme} />
+      <Contaier>
+        <Search />
+      </Contaier>
+    </>
+  )
 }
 
 export default Dashboard
+
+const Contaier = styled.main`
+  background-color: ${(props) => props.theme.background};
+  min-height: calc(100vh - 88px);
+`
