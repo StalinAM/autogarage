ea7fa6a41d57112647739acd321472da21e92212
Stalin <sta_vla411@hotmail.com>
2023-04-02 18:33:09 -0500

    add auth and carInformation context

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import Auth from './context/Auth'
+import CarInformation from './context/CarInformation'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Auth>
+      <CarInformation>
+        <App />
+      </CarInformation>
+    </Auth>
   </BrowserRouter>
 )
