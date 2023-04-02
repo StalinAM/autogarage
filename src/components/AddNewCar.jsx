3ab6b48b301caf2216bb55cf2a1d9aa079ebd4c9
Stalin <sta_vla411@hotmail.com>
2023-04-02 18:28:41 -0500

    add new info car

--- src/components/AddNewCar.jsx
+++ src/components/AddNewCar.jsx
@@ -1,13 +1,52 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
 import FormCar from './FormCar'
+import { AuthContext } from '../context/Auth'
+import { insertNewCar } from '../firebase/services'
+import { CarInformationContext } from '../context/CarInformation'
 
 function AddNewCar() {
+  const { currentUser } = useContext(AuthContext)
+  const { loadCarListData } = useContext(CarInformationContext)
   const [active, setActive] = useState(false)
+  const [infoCar, setInfoCar] = useState({
+    licensePlate: '',
+    name: '',
+    idPerson: '',
+    contract: ''
+  })
+  const addCar = async () => {
+    if (
+      infoCar.licensePlate &&
+      infoCar.name &&
+      infoCar.idPerson &&
+      infoCar.contract
+    ) {
+      const newCar = {
+        uid: currentUser.uid,
+        ...infoCar
+      }
+      await insertNewCar({ ...newCar })
+    }
+    loadCarListData()
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    addCar()
+    setActive(false)
+    setInfoCar({ licensePlate: '', name: '', idPerson: '', contract: '' })
+  }
+
   return (
     <>
       <NewCar onClick={() => setActive(true)}>Nuevo auto</NewCar>
-      <FormCar active={active} setActive={setActive} />
+      <FormCar
+        handleSubmit={handleSubmit}
+        infoCar={infoCar}
+        setInfoCar={setInfoCar}
+        active={active}
+        setActive={setActive}
+      />
     </>
   )
 }
@@ -15,7 +54,10 @@ function AddNewCar() {
 export default AddNewCar
 
 const NewCar = styled.button`
-  padding: 0.75rem 1rem;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 10px 20px 10px 20px;
   border-radius: 9px;
   color: ${(props) => props.theme.text};
   font-weight: 400;
