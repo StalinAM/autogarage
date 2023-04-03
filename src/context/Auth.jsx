a9c856931c8dd0a8de17d2051f0a96cd52eb9a7c
Stalin <sta_vla411@hotmail.com>
2023-04-03 12:20:56 -0500

    remove useRef

--- src/context/Auth.jsx
+++ src/context/Auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { auth } from '../firebase/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 import Loader from '../components/Loader'
