d8d283094022f29d53d72e85ad89898c0666b0d7
Stalin <sta_vla411@hotmail.com>
2023-04-03 12:18:46 -0500

    add route protection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { useContext } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { dark, light } from './styles/Theme'
 import GlobalStyles from './styles/GlobalStyles'
@@ -6,18 +7,50 @@ import useThemeMode from './hooks/useThemeMode'
 import Dashboard from './routes/Dashboard'
 import SignIn from './routes/SignIn'
 import SignUp from './routes/SignUp'
+import { AuthContext } from './context/Auth'
 function App() {
   const [theme, toggleTheme] = useThemeMode()
+  const { currentUser } = useContext(AuthContext)
+  const ProtectDashboard = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to='/' />
+    }
+    return children
+  }
+  const ProtectLogin = ({ children }) => {
+    if (currentUser) {
+      return <Navigate to='/dashboard' />
+    }
+    return children
+  }
   return (
     <>
       <GlobalStyles theme={theme === 'dark' ? dark : light} />
       <ThemeProvider theme={theme === 'dark' ? dark : light}>
         <Routes>
-          <Route path='/' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
+          <Route
+            path='/'
+            element={
+              <ProtectLogin>
+                <SignIn />
+              </ProtectLogin>
+            }
+          />
+          <Route
+            path='/signup'
+            element={
+              <ProtectLogin>
+                <SignUp />
+              </ProtectLogin>
+            }
+          />
           <Route
             path='/dashboard'
-            element={<Dashboard theme={theme} toggleTheme={toggleTheme} />}
+            element={
+              <ProtectDashboard>
+                <Dashboard theme={theme} toggleTheme={toggleTheme} />
+              </ProtectDashboard>
+            }
           />
         </Routes>
       </ThemeProvider>
